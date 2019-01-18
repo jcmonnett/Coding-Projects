b9270d845644812924fca687364444ef4d452e11
jcmonnett <17549675+jcmonnett@users.noreply.github.com>
2019-01-17 23:13:15 -0500

    Changes1

---
+++ Test1.js
@@ -0,0 +1,49 @@
+/*
+
+var firstName = "John";
+var age = 28;
+
+//Type Coecerion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' years old' + job + '. Is he married? ' + isMarried);
+
+
+//variabe mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' years old' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+//Basic Operators
+
+var currentyear, yearJohn, yearMark;
+ageJohn = 28;
+ageMark = 33;
+now = 2019;
+yearJohn = now - 28;
+yearMark = now - 33;
+
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+//Logical Operators
+
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+//typeof operator
+
+console.log(typeof johnOlder);
